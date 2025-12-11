a1df05e70332ee1a714345a14770cc2a4613b4f1
Adrián Gallardo <adriangdp@gmail.com>
2025-12-11 19:53:19 +0100

    refactor: Flattened tailwind classes and destructured props. Placed button and onClick navigate as parent for better semantics and usability.

--- src/components/cards/film-card.jsx
+++ src/components/cards/film-card.jsx
@@ -1,72 +1,42 @@
 import { useNavigate,useLocation } from 'react-router-dom'
 import { pathImageOriginal } from '../../api/apiRoutes'
-import { useCredentials } from '../../stores/authStore';
 import Score from '../ui/score.component';
 
 import posterPlaceholder from '@assets/missing_poster.webp'
 
 
-const FilmCard = ({data:film, children}) =>{
+const FilmCard = ({data, children}) =>{
 
     const navigate = useNavigate();
     const {pathname} = useLocation();
-    const {user} = useCredentials();
+    const {id,vote_average,poster_path,title} = {...data}
     return(
-        <div className='
-            group 
-            md:mx-0
-            mt-6
-            snap-center   
-            w-[75vw]
-            md:w-[18em]    
-            h-fit             
-            cursor-pointer     
-            flex-none
-            relative
-            overflow-visible
-            z-2
-        '> 
-            <div className='absolute right-1 -top-6 lg:-top-2 w-0 h-full lg:-translate-y-4 flex flex-col items-center overflow-visible z-20'>
-                <Score score={film.vote_average}></Score>
+        <button className='plainbtn relative group md:mx-0 mt-6 snap-center w-[75vw] md:w-[18em] h-fit cursor-pointer flex-none overflow-visible'> 
+            <div className='z-2 absolute right-1 -top-6 lg:-top-2 w-0 h-full lg:-translate-y-4 flex flex-col items-center overflow-visible'>
+                <Score score={vote_average}></Score>
                 {children}
             </div>           
-            <div onClick={() => {
+            <div onClick={() => {1 
                 navigate(
-                    `/films/${film.id}`,
+                    `/films/${id}`,
                     {
                         state:{from: pathname}
                     }
                 )}}
-                className='
-                    flex
-                    flex-col                
+                className='flex flex-col                
                 '
             >                     
                     <img 
-                        src={film.poster_path === null || film.poster_path.length < 1 ? posterPlaceholder : pathImageOriginal+film.poster_path}
-                        className='                            
-                            aspect-poster
-                            bg-cover
-                            bg-center
-                            bg-no-repeat
-                            rounded-lg
-                            object-cover
-                            transition-transform
-                            group-hover:scale-105
-                            '
-                            title={`${film.title} film poster`}
+                        src={poster_path === null || poster_path.length < 1 ? posterPlaceholder : pathImageOriginal+poster_path}
+                        className='aspect-poster bg-cover bg-center bg-no-repeat rounded-lg object-cover transition-transform group-hover:scale-105'
+                            title={`${title} film poster`}
                     />
                 
             </div>
-                <p className=' 
-                    mt-3                                                                 
-                    text-3xl
-                    lg:text-4xl 
-                    font-headers                             
-                '>
-                    {film.title}
-                </p>
-        </div>      
+            <p className='mt-3 text-2xl lg:text-4xl font-headers text-start line-clamp-2'>
+                {title}
+            </p>
+        </button>      
     )
 }
 
