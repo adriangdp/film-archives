8d652d98aef59cea316f7fb176a0f020669e7f94
Adrián Gallardo <adriangdp@gmail.com>
2025-12-11 21:03:40 +0100

    refactor: new styling for <a> coming from main.style.css. NavbarLink is flattened and takes global values where appropiate.

--- src/components/navbar/navbar-link.component.jsx
+++ src/components/navbar/navbar-link.component.jsx
@@ -3,19 +3,10 @@ import { Link } from 'react-router-dom';
 const NavBarLink = ({to:uri, children}) =>{
     return(
         <Link to={uri} 
-                    className='  
-                        py-2                   
-                        mx-3
-                        text-center
-                        md:text-start
-                        decoration-dotted
-                        hover:decoration-seat-number 
-                        hover:decoration-2 
-                        hover:underline 
-                        hover:underline-offset-6
-                '>
-                    {children}
-                </Link>
+            className='py-2 mx-3 text-center md:text-start hover:decoration-seat-number hover:underline'
+        >
+            {children}
+        </Link>
     );
 }
 
